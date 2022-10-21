32806a7a041a267902530ffeb6840f5c84596f94
hungphiitfpt <110955219+hungphiitfpt@users.noreply.github.com>
2022-10-21 10:39:48 +0700

    Add files via upload

--- FireBase/index.js
+++ FireBase/index.js
@@ -11,7 +11,8 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 function uploadImage() {
-    const ref = firebase.storage().ref();
+    
+    const ref = firebase.storage().ref("Files/" + "Hungphi/");
     // Lấy phẩn tử đầu tiên
     const file = document.querySelector('#photo').files[0]
     const metadata = {
@@ -25,3 +26,67 @@ function uploadImage() {
         console.log(url);
     }).catch(console.error)
 }
+let a = [];
+const getRandomImages = () => {
+
+    const storageRef = firebase.storage().ref("Files/" + "Hungphi");
+
+    storageRef.listAll().then((result) => {
+      result.items.forEach((imageRef) => {
+        imageRef.getDownloadURL().then((url) => {
+          console.log(url);
+          a.push(url)
+          console.log(a);
+        })
+       
+      }) 
+      
+    })       
+  }
+
+
+//Google singin provider
+var ggProvider = new firebase.auth.GoogleAuthProvider();
+//Facebook singin provider
+var fbProvider = new firebase.auth.FacebookAuthProvider();
+//Login in variables
+const btnGoogle = document.getElementById('btnGoogle');
+const btnFaceBook = document.getElementById('btnFacebook');
+
+    //Sing in with Google
+    btnGoogle.addEventListener('click', e => {
+        firebase.auth().signInWithPopup(ggProvider).then(function(result) {
+            var token = result.credential.accessToken;
+            var user = result.user;
+            console.log('User>>Goole>>>>', user);
+            userId = user.uid;
+
+        }).catch(function(error) {
+            console.error('Error: hande error here>>>', error.code)
+        })
+    }, false)
+
+    //Sing in with Facebook
+    btnFaceBook.addEventListener('click', e => {
+        firebase.auth().signInWithPopup(fbProvider).then(function(result) {
+            // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+            var token = result.credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+            console.log('User>>Facebook>', user);
+            // ...
+            userId = user.uid;
+        
+        }).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+            console.error('Error: hande error here>Facebook>>', error.code)
+        });
+    }, false)
+    //jquery 
